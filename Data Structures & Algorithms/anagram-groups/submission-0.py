842869eaf9d4bdf679934642fe810b24164b9e6a
Joyce Nimely <nimelyj@berea.edu>
2026-08-29 21:44:05 -0500

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.py
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -1,11 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram_map = defaultdict(list)  # Dictionary to hold grouped anagrams
-
-        for word in strs:
-            # Sort the word to use as a key
-            key = ''.join(sorted(word))
-            anagram_map[key].append(word)  # Group all words with the same sorted key
-
-        return list(anagram_map.values())
-        
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
