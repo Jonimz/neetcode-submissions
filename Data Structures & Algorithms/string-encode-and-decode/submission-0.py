c5e28c394d5694b34b08dfcebc3728f3806c5bbf
Joyce Nimely <nimelyj@berea.edu>
2026-09-03 20:33:12 -0500

    Update: string-encode-and-decode - submission-0

--- Data Structures & Algorithms/string-encode-and-decode/submission-0.py
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -1,40 +1,25 @@
 class Solution:
 
-
     def encode(self, strs: List[str]) -> str:
-
-
-        res = ""   # general string we are going to return 
-
+        res = []
         for s in strs:
-
-            res += str(len(s)) + "#" + s
-
-        return res 
-
+            res.append(str(len(s)))
+            res.append("#")
+            res.append(s)
+        return "".join(res)
 
     def decode(self, s: str) -> List[str]:
-
-        # the s here represents the list of strings that we want to return 
-
-        final_res = []
-
-        i = 0 
+        res = []
+        i = 0
 
         while i < len(s):
-            j = i 
-            while s[j] != "#":
-                j += 1 
-
+            j = i
+            while s[j] != '#':
+                j += 1
             length = int(s[i:j])
-
-            i = j + 1 
-
+            i = j + 1
             j = i + length
-
-            final_res.append(s[i : j])
-            # let's continue our iteration
+            res.append(s[i:j])
             i = j
 
-        return final_res
-
+        return res
